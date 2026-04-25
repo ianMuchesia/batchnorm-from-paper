31d5730288f4cb4c20e12a513f98cd13eed879c1
msodoki <ianmuchesia@yahoo.com>
2026-04-25 19:39:22 +0300

    add batchnorm backward pass and gradient-check notebook updates

--- src/batchnorm.py
+++ src/batchnorm.py
@@ -12,12 +12,12 @@ class BatchNorm:
     def forward(self,x):
         
         
+        self.x = x
+        self.batch_mean = np.mean(x,axis=0,keepdims=True)
         
-        batch_mean = np.mean(x,axis=0,keepdims=True)
+        self.batch_variance = np.var(x,axis=0,keepdims=True)
         
-        batch_variance = np.var(x,axis=0,keepdims=True)
-        
-        normalized = (x - batch_mean) / np.sqrt(batch_variance + self.epsilon)
+        self.normalized = (x - self.batch_mean) / np.sqrt(self.batch_variance + self.epsilon)
         
         if(self.gamma is None):
             num_features = x.shape[1]
@@ -26,12 +26,39 @@ class BatchNorm:
             self.beta = np.zeros((1,num_features))
         
         
-        scaled_x =  normalized * self.gamma + self.beta
+        scaled_x =  self.normalized * self.gamma + self.beta
         
         return scaled_x
-
+    
+    
+    def backward(self,dout):
+        
+        d_normalized = dout * self.gamma
+        d_beta = dout
+        d_gamma = dout * self.normalized
         
         
+        print(f"The shape of d_normalized: {d_normalized.shape}")
         
+        inv_std = 1/ np.sqrt(self.batch_variance + self.epsilon)
+        
+        print(f"The shape of batch_mean : {self.batch_mean.shape}")
+        
+        d_var = np.sum(d_normalized * ( self.x -self.batch_mean) * -0.5 * (inv_std**3),axis=0)
+        
+        print(f"The shape of d_var : {d_var.shape}")
+        
+        d_mean = np.sum(d_normalized* - (self.batch_variance + self.epsilon)**-0.5,axis = 0) + d_var * -2/self.x.shape[0] * np.sum(self.x - self.batch_mean,axis=0)
+        
+        
+        print(f"The shape of d_mean :{d_mean.shape}")
+        
+        d_x = d_normalized* (self.batch_variance + self.epsilon)**-0.5 + d_var * 2/self.x.shape[0] * (self.x - self.batch_mean) + d_mean * 1/self.x.shape[0]
+        
+        dx_beta_analytical = np.sum(dout,axis=0,keepdims=True)
+        
+        dx_gamma_analytical = np.sum(d_gamma,axis=0,keepdims=True)
+        
+        return d_x,dx_beta_analytical,dx_gamma_analytical
         
     
